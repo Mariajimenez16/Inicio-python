d7dda42b669141fafc7caf3ad2af30ac74bee290
Maria Jiménez <maria.jimenezs@upb.edu.co>
2026-02-09 19:15:29 -0500

    sumar dos numeros fijos

--- primer_algoritmo.py
+++ primer_algoritmo.py
@@ -1,1 +1,9 @@
-print("hola mundo")
+#print("hola mundo")
+
+#sumar dos numeros fijos
+numero1 = 5
+numero2 = 10
+suma = numero1 + numero2
+print("La suma de", numero1, "y", numero2, "es:", suma)
+
+
