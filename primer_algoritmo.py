bc78525ce39721135849428a52c5ecfd68ecb424
Maria Jiménez <maria.jimenezs@upb.edu.co>
2026-02-09 19:20:15 -0500

    multiplicacion

--- primer_algoritmo.py
+++ primer_algoritmo.py
@@ -6,4 +6,9 @@ numero2 = 10
 suma = numero1 + numero2
 print("La suma de", numero1, "y", numero2, "es:", suma)
 
+#multiplicar dos numeros fijos
+numero3 = 7
+numero4 = 5
+multiplicacion = numero3 * numero4
+print("la multiplicacion de", numero3, "y", numero4, "es:", multiplicacion)
 
