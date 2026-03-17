4c9c3ff0fb76ba145aea62b7e5efe0d4e632181d
Maria Jiménez <maria.jimenezs@upb.edu.co>
2026-03-16 18:48:55 -0700

    ejercicio 2 sobre factorizar un n+umero

--- taller_condicionales.py
+++ taller_condicionales.py
@@ -10,3 +10,12 @@ while codigo != "1" or contrasena != "1234":
         print("Datos incorrectos, intente de nuevo")
 
 print("Acceso aceptado")
+
+#ejercicio 2: factorial 
+numero = int(input("Ingrese un número positivo: "))
+resultado = 1
+
+for i in range(1, numero + 1):
+    resultado = resultado * i
+
+print("El factorial es:", resultado)
